3c8f371d390e0d7d0287d812e6317d7715b7e16a
saurabhpal85 <saurabhpal85@github.com>
2019-11-21 13:44:47 +0000

    added user input and promt

--- practice/src/main/java/practice/FlipTheBit.java
+++ practice/src/main/java/practice/FlipTheBit.java
@@ -1,47 +1,31 @@
 	package practice;
-	
-	import java.util.Stack;
-	
-	public class FlipTheBit {
+
+import java.util.Scanner;
+
+public class FlipTheBit {
 	
 		public static void main(String[] args) {
-			
-			int a = 32, b = 1;
-			FlipTheBit.invertSpecificBit(a,b);
+			int num;
+			int bit;
+			System.out.println("Enter the Number \n");
+			Scanner scanner = new Scanner(System.in);
+			num = scanner.nextInt();
+			System.out.println("Enter the bit to flip \n");
+			Scanner scanner1 = new Scanner(System.in);
+			bit = scanner1.nextInt();
+			FlipTheBit.invertSpecificBit(num, bit);
 			
 		}
 	
 		
 		 static void invertSpecificBit(int num, int bit) 
-		    { 
-		        String binaryString = Integer.toBinaryString(num);
-		        System.out.println("initial binary string : "+ binaryString);
-		        char[] charArray = binaryString.toCharArray();
-		        Stack<Character> bitStack = new Stack<Character>();
-		        
-		        for(char ch: charArray) {
-		        	
-		        	bitStack.push(ch);
-		        }
-		        
-		        int count = 0;
-		        int convertedValue=0;
-		        String finalBinaryString = "";
-		        
-		        for (int i = bitStack.size()-1; i >=0; i --) {
-					count ++;
-					int value = Character.getNumericValue(bitStack.get(i));
-					if(count==bit) {
-						  value ^= 1;
-					}
-					
-					finalBinaryString = String.valueOf(value)+ finalBinaryString;
-					
-		        }
-		        
-		        System.out.println("final Binary String : "+ finalBinaryString);
-		        convertedValue = Integer.parseInt(finalBinaryString, 2);
-		        System.out.println(convertedValue); 
+		    {
+			 
+			 System.out.println("Before "+num); 
+			 System.out.println("Before " + Integer.toBinaryString(num)); 
+			 num ^= (1<<(bit-1));
+			 System.out.println("After" + Integer.toBinaryString(num));
+			 System.out.println("After "+num); 
 		    } 
 		 
 	}
